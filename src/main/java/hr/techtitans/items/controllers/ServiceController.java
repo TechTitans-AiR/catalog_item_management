51ddca165afef3583a56a27d0cf7bc81c91d8d1f
Ivan Ercegovac <iercegova@foi.hr>
2023-12-07 11:56:34 +0100

    Deleting products (items) and services

--- src/main/java/hr/techtitans/items/controllers/ServiceController.java
+++ src/main/java/hr/techtitans/items/controllers/ServiceController.java
@@ -1,16 +1,16 @@
 package hr.techtitans.items.controllers;
 
+import hr.techtitans.items.dtos.ItemDto;
 import hr.techtitans.items.dtos.ServiceDto;
 import hr.techtitans.items.services.ItemService;
 import hr.techtitans.items.services.ServiceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/services")
@@ -22,5 +22,32 @@ public class ServiceController {
     @GetMapping
     public ResponseEntity<List<ServiceDto>> getAllServices(){
         return new ResponseEntity<List<ServiceDto>>(serviceService.allServices(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{serviceId}")
+    public ResponseEntity<?> getServiceById(@PathVariable String serviceId){
+        try {
+            ServiceDto serviceDto = serviceService.getServiceById(serviceId);
+
+            if (serviceDto != null) {
+                return new ResponseEntity<>(serviceDto, HttpStatus.OK);
+            } else {
+                String errorMessage = "Service with id: " + serviceId + " is not found.";
+                return new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception ex) {
+
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/delete/{serviceId}")
+    public ResponseEntity<Object> deleteService(@PathVariable String serviceId) {
+        return serviceService.deleteServiceById(serviceId);
+    }
+    @DeleteMapping("/delete/")
+    public ResponseEntity<Object> noServiceIdProvided() {
+        Map<String, Object> responseBody = Map.of("message", "Please provide an service ID");
+        return new ResponseEntity<>(responseBody, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/hr/techtitans/items/services/ItemService.java
+++ src/main/java/hr/techtitans/items/services/ItemService.java
@@ -7,10 +7,15 @@ import hr.techtitans.items.models.Item;
 import hr.techtitans.items.models.ItemCategories;
 import hr.techtitans.items.repositories.ItemRepository;
 import hr.techtitans.items.repositories.ItemCategoriesRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -42,7 +47,45 @@ public class ItemService {
                 item.getMaterial(),
                 item.getBrand(),
                 item.getCurrency()
-                );
+        );
+    }
+
+    public ItemDto getItemById(String userId) {
+        ObjectId objectId = new ObjectId(userId);
+        Optional<Item> optionalItem = itemRepository.findById(objectId);
+
+        if (optionalItem.isPresent()) {
+            Item item = optionalItem.get();
+            return mapToItemDto(item);
+        } else {
+            return null;
+        }
+    }
+
+
+    public ResponseEntity<Object> deleteItemById(String itemId) {
+        try {
+            if (itemId == null || itemId.isEmpty()) {
+                return new ResponseEntity<>("Item ID not provided", HttpStatus.BAD_REQUEST);
+            }
+            ObjectId objectId = new ObjectId(itemId);
+            if (itemRepository.existsById(objectId)) {
+                itemRepository.deleteById(objectId);
+                Map<String, Object> responseBody = Map.of("message", "Item deleted successfully");
+                return new ResponseEntity<>(responseBody, HttpStatus.OK);
+            } else {
+                Map<String, Object> responseBody = Map.of("message", "Item not found");
+                return new ResponseEntity<>(responseBody, HttpStatus.NOT_FOUND);
+            }
+
+        } catch (IllegalArgumentException e) {
+            Map<String, Object> responseBody = Map.of("message", "Item not found");
+            return new ResponseEntity<>(responseBody, HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Map<String, Object> responseBody = Map.of("message", "An error occurred");
+            return new ResponseEntity<>(responseBody, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
 
